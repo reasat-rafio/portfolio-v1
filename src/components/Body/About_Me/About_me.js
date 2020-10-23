0b1e44f7f0b8faa64749bc7a9fe9043a7bc2ec15
reasat-rafio <rafioar96@gmail.com>
2020-10-23 14:39:27 +0600

    fix1

--- src/components/Body/About_Me/About_me.js
+++ src/components/Body/About_Me/About_me.js
@@ -6,78 +6,79 @@ import { motion } from "framer-motion";
 import Rectangle from "./Rectangle";
 
 const About_me = () => {
-  const [active, setActive] = useState(false);
+   const [active, setActive] = useState(false);
 
-  // section will be visible on scroll
-  useEffect(() => {
-    window.addEventListener("scroll", () => {
-      const position = document.querySelector(".about_me");
-      let topPosition = position.getBoundingClientRect();
-      if (window.scrollY >= topPosition.top - 100) {
-        setActive(true);
-      } else {
-        setActive(false);
-      }
-    });
-  }, [active]);
+   // section will be visible on scroll
+   useEffect(() => {
+      window.addEventListener("scroll", () => {
+         const position = document.querySelector(".about_me");
+         let topPosition = position.getBoundingClientRect();
+         if (window.scrollY >= topPosition.top - 100) {
+            setActive(true);
+         } else {
+            setActive(false);
+         }
+      });
+   }, [active]);
 
-  return (
-    <motion.div
-      initial={{ opacity: 0, y: 30 }}
-      animate={{ opacity: active ? 1 : 0, y: active ? 0 : 30 }}
-      transition={{
-        opacity: { duration: 0.5, ease: "easeInOut" },
-        y: { duration: 0.5, ease: "easeInOut" },
-      }}
-      id="about"
-      className="about_me"
-    >
-      <div className="inner">
-        <section>
-          <div className="about_header">
-            <h2>About Me</h2>
-            <div className="divider" />
-          </div>
-          <div className="details">
-            <Grid spacing={5} container>
-              <Grid className="inner__details" item sm={7}>
-                <p>
-                  Hello! I'm Rafio, a self-taught fullstack Web Developer from
-                  Dhaka, BD.
-                </p>
-                <p>
-                  I enjoy creating things that live on the internet. My goal is
-                  to always build products that provide pixel-perfect,
-                  performant experiences.
-                </p>
-                <p>
-                  I always had passion for computer and i am good with problem
-                  solving so i start learning programming. I love what i do.
-                </p>
-                <p>
-                  Here are a few technologies I've been working with recently:
-                </p>
-                <ul className="skills_list">
-                  <li>JavaScript (ES6+)</li>
-                  <li>HTML & (S)CSS</li>
-                  <li>React</li>
-                  <li>Node.js</li>
-                  <li>Strapi</li>
-                </ul>
-              </Grid>
-              <div className="img">
-                <Grid sm={5} item>
-                  <img src={img} alt="" />
-                  <div className="img_overlay" />
-                  <Rectangle />
-                </Grid>
-              </div>
-            </Grid>
-          </div>
-        </section>
-      </div>
-    </motion.div>
-  );
+   return (
+      <motion.div
+         initial={{ opacity: 0, y: 30 }}
+         animate={{ opacity: active ? 1 : 0, y: active ? 0 : 30 }}
+         transition={{
+            opacity: { duration: 0.5, ease: "easeInOut" },
+            y: { duration: 0.5, ease: "easeInOut" },
+         }}
+         id="about"
+         className="about_me"
+      >
+         <div className="inner">
+            <section>
+               <div className="about_header">
+                  <h2>About Me</h2>
+                  <div className="divider" />
+               </div>
+               <div className="details">
+                  <Grid spacing={5} container>
+                     <Grid className="inner__details" item sm={7}>
+                        <p>
+                           Hello! I'm Rafio, a self-taught fullstack Web
+                           Developer from Dhaka, BD.
+                        </p>
+                        <p>
+                           I am currently studying computer science at United
+                           International University as a freshman.
+                        </p>
+                        <p>
+                           I enjoy creating things that live on the internet. My
+                           goal is to always build products that provide
+                           pixel-perfect, performant experiences.
+                        </p>
+                        <p>
+                           Here are a few technologies I've been working with
+                           recently:
+                        </p>
+                        <ul className="skills_list">
+                           <li>JavaScript (ES6+)</li>
+                           <li>HTML & (S)CSS</li>
+                           <li>React</li>
+                           <li>Node.js</li>
+                           <li>Strapi</li>
+                        </ul>
+                     </Grid>
+                     <div className="img">
+                        <Grid sm={5} item>
+                           <img src={img} alt="" />
+                           <div className="img_overlay" />
+                           <Rectangle />
+                        </Grid>
+                     </div>
+                  </Grid>
+               </div>
+            </section>
+         </div>
+      </motion.div>
+   );
 };
 
 export default About_me;
--- src/components/Body/I_Have_Built/IHaveBuilt.js
+++ src/components/Body/I_Have_Built/IHaveBuilt.js
@@ -5,125 +5,145 @@ import ExitToAppSharpIcon from "@material-ui/icons/ExitToAppSharp";
 import { motion } from "framer-motion";
 
 const IHaveBuilt = () => {
-  const [header, setHeader] = useState(false);
+   const [header, setHeader] = useState(false);
 
-  // section will be visible on scroll
-  // header animation
-  useEffect(() => {
-    window.addEventListener("scroll", () => {
-      const position = document.querySelector(".have_built_header");
-      let topPosition = position.getBoundingClientRect();
+   // section will be visible on scroll
+   // header animation
+   useEffect(() => {
+      window.addEventListener("scroll", () => {
+         const position = document.querySelector(".have_built_header");
+         let topPosition = position.getBoundingClientRect();
 
-      if (window.scrollY >= topPosition.top + 700) {
-        setHeader(true);
-      } else {
-        setHeader(false);
-      }
-    });
-  }, [header]);
+         if (window.scrollY >= topPosition.top + 700) {
+            setHeader(true);
+         } else {
+            setHeader(false);
+         }
+      });
+   }, [header]);
 
-  return (
-    <motion.div
-      initial={{ opacity: 0, y: 90 }}
-      animate={{ opacity: header ? 1 : 0, y: header ? 0 : 30 }}
-      transition={{
-        opacity: { duration: 0.5, ease: "easeInOut" },
-        y: { duration: 0.5, ease: "easeInOut" },
-      }}
-      id="work"
-      className="i_have_build"
-    >
-      <div className="inner">
-        <section>
-          <div className="have_built_header">
-            <h2 className="have_built_header_h2">Some Things I've Built</h2>
-            <div className="divider" />
-          </div>
+   return (
+      <motion.div
+         initial={{ opacity: 0, y: 90 }}
+         animate={{ opacity: header ? 1 : 0, y: header ? 0 : 30 }}
+         transition={{
+            opacity: { duration: 0.5, ease: "easeInOut" },
+            y: { duration: 0.5, ease: "easeInOut" },
+         }}
+         id="work"
+         className="i_have_build"
+      >
+         <div className="inner">
+            <section>
+               <div className="have_built_header">
+                  <h2 className="have_built_header_h2">
+                     Some Things I've Built
+                  </h2>
+                  <div className="divider" />
+               </div>
 
-          <div className="details">
-            <Grid container className="inner_details topSectionPositon">
-              <Grid item lg={6} sm={12} xs={12}>
-                <div
-                  onClick={() =>
-                    (window.location.href = "https://red-onion71.netlify.app")
-                  }
-                  className="img_pj1"
-                />
-              </Grid>
-              <Grid item lg={6} sm={12} className="inner_details_text top">
-                <p className="project_overline">Featured Project</p>
-                <h3 className="project-title">Red Onion</h3>
-                <div className="project_description">
-                  <p>
-                    A nicer look at your GitHub profile and repository stats
-                    with data visualizations of your top languages and stars.
-                    Sort through your top repos by number of stars, forks, and
-                    size.
-                  </p>
-                </div>
-                <ul>
-                  <Button className="btn">React & Redux</Button>
-                  <Button className="btn">Strapi</Button>
-                  <Button className="btn">Metarial UI</Button>
-                </ul>
+               <div className="details">
+                  <Grid container className="inner_details topSectionPositon">
+                     <Grid item lg={6} sm={12} xs={12}>
+                        <div
+                           onClick={() =>
+                              (window.location.href =
+                                 "https://red-onion71.netlify.app")
+                           }
+                           className="img_pj1"
+                        />
+                     </Grid>
+                     <Grid
+                        item
+                        lg={6}
+                        sm={12}
+                        className="inner_details_text top"
+                     >
+                        <p className="project_overline">Featured Project</p>
+                        <h3 className="project-title">Red Onion</h3>
+                        <div className="project_description">
+                           <p>
+                              A nicer look at your GitHub profile and repository
+                              stats with data visualizations of your top
+                              languages and stars. Sort through your top repos
+                              by number of stars, forks, and size.
+                           </p>
+                        </div>
+                        <ul>
+                           <Button className="btn">React & Redux</Button>
+                           <Button className="btn">Strapi</Button>
+                           <Button className="btn">Metarial UI</Button>
+                        </ul>
 
-                <div className="proeject-links">
-                  <a
-                    target="_blank"
-                    href="https://github.com/reasat-rafio/Red-onion.git"
-                  >
-                    <GitHubIcon className="icon" />
-                  </a>
-                  <a target="_blank" href="https://red-onion71.netlify.app">
-                    <ExitToAppSharpIcon className="icon" />
-                  </a>
-                </div>
-              </Grid>
-            </Grid>
-            <Grid container className="inner_details">
-              <Grid item lg={6} sm={12} className="inner_details_text bottom">
-                <p className="project_overline">Featured Project</p>
-                <h3 className="project-title">Red Onion</h3>
-                <div className="project_description">
-                  <p>
-                    A nicer look at your GitHub profile and repository stats
-                    with data visualizations of your top languages and stars.
-                    Sort through your top repos by number of stars, forks, and
-                    size.
-                  </p>
-                </div>
-                <ul>
-                  <Button className="btn">React</Button>
-                  <Button className="btn">Node</Button>
-                  <Button className="btn">Express</Button>
-                  <Button className="btn">Ant Design</Button>
-                </ul>
-                <div className="proeject-links">
-                  <a
-                    target="_blank"
-                    href="https://github.com/reasat-rafio/travel_site.git"
-                  >
-                    <GitHubIcon className="icon" />
-                  </a>
-                  <a target="_blank" href="https://travel-bd1.herokuapp.com/">
-                    <ExitToAppSharpIcon className="icon" />
-                  </a>
-                </div>
-              </Grid>
-              <Grid item lg={6} sm={12} xs={12}>
-                <div
-                  onClick={() =>
-                    (window.location.href = "https://travel-bd1.herokuapp.com")
-                  }
-                  className="img_pj2"
-                />
-              </Grid>
-            </Grid>
-          </div>
-        </section>
-      </div>
-    </motion.div>
-  );
+                        <div className="proeject-links">
+                           <a
+                              target="_blank"
+                              href="https://github.com/reasat-rafio/Red-onion.git"
+                           >
+                              <GitHubIcon className="icon" />
+                           </a>
+                           <a
+                              target="_blank"
+                              href="https://red-onion71.netlify.app"
+                           >
+                              <ExitToAppSharpIcon className="icon" />
+                           </a>
+                        </div>
+                     </Grid>
+                  </Grid>
+                  <Grid container className="inner_details">
+                     <Grid
+                        item
+                        lg={6}
+                        sm={12}
+                        className="inner_details_text bottom"
+                     >
+                        <p className="project_overline">Featured Project</p>
+                        <h3 className="project-title">Red Onion</h3>
+                        <div className="project_description">
+                           <p>
+                              A nicer look at your GitHub profile and repository
+                              stats with data visualizations of your top
+                              languages and stars. Sort through your top repos
+                              by number of stars, forks, and size.
+                           </p>
+                        </div>
+                        <ul>
+                           <Button className="btn">React</Button>
+                           <Button className="btn">Node</Button>
+                           <Button className="btn">Express</Button>
+                           <Button className="btn">Ant Design</Button>
+                        </ul>
+                        <div className="proeject-links">
+                           <a
+                              target="_blank"
+                              href="https://github.com/reasat-rafio/travel_site.git"
+                           >
+                              <GitHubIcon className="icon" />
+                           </a>
+                           <a
+                              target="_blank"
+                              href="https://travel-bd1.herokuapp.com/"
+                           >
+                              <ExitToAppSharpIcon className="icon" />
+                           </a>
+                        </div>
+                     </Grid>
+                     <Grid item lg={6} sm={12} xs={12}>
+                        <div
+                           onClick={() =>
+                              (window.location.href =
+                                 "https://travel-bd1.herokuapp.com")
+                           }
+                           className="img_pj2"
+                        />
+                     </Grid>
+                  </Grid>
+               </div>
+            </section>
+         </div>
+      </motion.div>
+   );
 };
 
 export default IHaveBuilt;
